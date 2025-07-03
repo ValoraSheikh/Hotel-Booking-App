8bcbd1fb7d24a3ad27648b9b118230637d106023
Aman Sheikh <haarishsheikh04@gmail.com>
2025-07-03 11:31:17 +0530

    Complete design of About us page

---
+++ app/(app)/about-us/page.tsx
@@ -0,0 +1,224 @@
+import Image from "next/image";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <section className="py-24 relative lg:px-32">
+        <div className="w-full max-w-7xl px-4 md:px-5 lg:px-5 mx-auto">
+          <div className="w-full justify-start items-center gap-12 grid lg:grid-cols-2 grid-cols-1">
+            <div className="w-full justify-center items-start gap-6 grid sm:grid-cols-2 grid-cols-1 lg:order-first order-last">
+              <div className="pt-24 lg:justify-center sm:justify-end justify-start items-start gap-2.5 flex">
+                <Image
+                  className=" rounded-xl object-cover"
+                  src="https://images.unsplash.com/photo-1563911302283-d2bc129e7570?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                  alt="about Us image"
+                  height={500}
+                  width={500}
+                />
+              </div>
+              <Image
+                className="sm:ml-0 ml-auto rounded-xl object-cover"
+                src="https://images.unsplash.com/photo-1573052905904-34ad8c27f0cc?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt="about Us image"
+                height={500}
+                width={500}
+              />
+            </div>
+            <div className="w-full flex-col justify-center lg:items-start items-center gap-10 inline-flex">
+              <div className="w-full flex-col justify-center items-start gap-8 flex">
+                <div className="w-full flex-col justify-start lg:items-start items-center gap-3 flex">
+                  <h2 className="text-gray-900 text-4xl font-bold font-manrope leading-normal lg:text-start text-center">
+                    Best Hotel in Jaisalmer
+                  </h2>
+                  <p className="text-gray-500 text-base font-normal leading-relaxed lg:text-start text-center">
+                    Every project we've undertaken has been a collaborative
+                    effort, where every person involved has left their mark.
+                    Together, we've not only constructed buildings but also
+                    built enduring connections that define our success story.
+                  </p>
+                </div>
+                <div className="w-full lg:justify-start justify-center items-center sm:gap-10 gap-5 inline-flex">
+                  <div className="flex-col justify-start items-start inline-flex">
+                    <h3 className="text-gray-900 text-4xl font-bold font-manrope leading-normal">
+                      33+
+                    </h3>
+                    <h6 className="text-gray-500 text-base font-normal leading-relaxed">
+                      Years of Experience
+                    </h6>
+                  </div>
+                  <div className="flex-col justify-start items-start inline-flex">
+                    <h4 className="text-gray-900 text-4xl font-bold font-manrope leading-normal">
+                      125+
+                    </h4>
+                    <h6 className="text-gray-500 text-base font-normal leading-relaxed">
+                      Successful Projects
+                    </h6>
+                  </div>
+                  <div className="flex-col justify-start items-start inline-flex">
+                    <h4 className="text-gray-900 text-4xl font-bold font-manrope leading-normal">
+                      52+
+                    </h4>
+                    <h6 className="text-gray-500 text-base font-normal leading-relaxed">
+                      Happy Clients
+                    </h6>
+                  </div>
+                </div>
+              </div>
+              <button className="sm:w-fit w-full px-3.5 py-2 bg-indigo-600 hover:bg-indigo-800 transition-all duration-700 ease-in-out rounded-lg shadow-[0px_1px_2px_0px_rgba(16,_24,_40,_0.05)] justify-center items-center flex">
+                <span className="px-1.5 text-white text-sm font-medium leading-6">
+                  Read More
+                </span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <div className="flex items-center justify-center py-4 md:py-8 flex-wrap">
+        <button
+          type="button"
+          className="text-blue-700 hover:text-white border border-blue-600 bg-white hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-full text-base font-medium px-5 py-2.5 text-center me-3 mb-3"
+        >
+          All categories
+        </button>
+        <button
+          type="button"
+          className="text-gray-900 border border-white hover:border-gray-200 bg-white focus:ring-4 focus:outline-none focus:ring-gray-300 rounded-full text-base font-medium px-5 py-2.5 text-center me-3 mb-3"
+        >
+          Shoes
+        </button>
+        <button
+          type="button"
+          className="text-gray-900 border border-white hover:border-gray-200 bg-white focus:ring-4 focus:outline-none focus:ring-gray-300 rounded-full text-base font-medium px-5 py-2.5 text-center me-3 mb-3"
+        >
+          Bags
+        </button>
+        <button
+          type="button"
+          className="text-gray-900 border border-white hover:border-gray-200 bg-white focus:ring-4 focus:outline-none focus:ring-gray-300 rounded-full text-base font-medium px-5 py-2.5 text-center me-3 mb-3"
+        >
+          Electronics
+        </button>
+        <button
+          type="button"
+          className="text-gray-900 border border-white hover:border-gray-200 bg-white focus:ring-4 focus:outline-none focus:ring-gray-300 rounded-full text-base font-medium px-5 py-2.5 text-center me-3 mb-3"
+        >
+          Gaming
+        </button>
+      </div>
+      <div className="grid grid-cols-2 md:grid-cols-3 gap-4 lg:px-32 px-5 py-10">
+        <div>
+          <Image
+            height={500}
+            width={500}
+            className="h-auto max-w-full rounded-lg"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg"
+            alt=""
+          />
+        </div>
+        <div>
+          <Image
+            height={500}
+            width={500}
+            className="h-auto max-w-full rounded-lg"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg"
+            alt=""
+          />
+        </div>
+        <div>
+          <Image
+            height={500}
+            width={500}
+            className="h-auto max-w-full rounded-lg"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg"
+            alt=""
+          />
+        </div>
+        <div>
+          <Image
+            height={500}
+            width={500}
+            className="h-auto max-w-full rounded-lg"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg"
+            alt=""
+          />
+        </div>
+        <div>
+          <Image
+            height={500}
+            width={500}
+            className="h-auto max-w-full rounded-lg"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg"
+            alt=""
+          />
+        </div>
+        <div>
+          <Image
+            height={500}
+            width={500}
+            className="h-auto max-w-full rounded-lg"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg"
+            alt=""
+          />
+        </div>
+        <div>
+          <Image
+            height={500}
+            width={500}
+            className="h-auto max-w-full rounded-lg"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg"
+            alt=""
+          />
+        </div>
+        <div>
+          <Image
+            height={500}
+            width={500}
+            className="h-auto max-w-full rounded-lg"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg"
+            alt=""
+          />
+        </div>
+        <div>
+          <Image
+            height={500}
+            width={500}
+            className="h-auto max-w-full rounded-lg"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg"
+            alt=""
+          />
+        </div>
+        <div>
+          <Image
+            height={500}
+            width={500}
+            className="h-auto max-w-full rounded-lg"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg"
+            alt=""
+          />
+        </div>
+        <div>
+          <Image
+            height={500}
+            width={500}
+            className="h-auto max-w-full rounded-lg"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg"
+            alt=""
+          />
+        </div>
+        <div>
+          <Image
+            height={500}
+            width={500}
+            className="h-auto max-w-full rounded-lg"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
