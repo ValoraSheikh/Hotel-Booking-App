ef6670686f895f44697e93bec625b9dbccb0320e
Aman Sheikh <haarishsheikh04@gmail.com>
2025-07-04 12:07:04 +0530

    Make some changes in folder structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import AboutUs from "./components/sections/AboutUs";
-import Blog from "./components/sections/Blog";
-import Hero from "./components/sections/Hero";
-import Room from "./components/sections/Room";
-import Services from "./components/sections/Services";
-import Testimonials from "./components/sections/Testimonials";
+import AboutUs from "../components/sections/AboutUs";
+import Blog from "../components/sections/Blog";
+import Hero from "../components/sections/Hero";
+import Room from "../components/sections/Room";
+import Services from "../components/sections/Services";
+import Testimonials from "../components/sections/Testimonials";
 
 export default function Home() {
   return (
