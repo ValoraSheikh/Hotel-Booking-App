707935f49421b31654f35a41878b85c7f1b4bc2e
Aman Sheikh <haarishsheikh04@gmail.com>
2025-07-04 12:32:19 +0530

    create db connect file and user model

---
+++ models/User.model.ts
@@ -0,0 +1,58 @@
+import { Schema, model, models } from 'mongoose';
+import bcrypt from "bcryptjs";
+
+export interface IUser{
+  name: string;
+  email: string;
+  password: string;
+  role: 'user' | 'admin';
+  image?: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const userSchema = new Schema<IUser>(
+  {
+    name: {
+      type: String,
+      required: [true, 'Name is required'],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: [true, 'Password is required'],
+      select: false, // Don't expose password by default
+    },
+    role: {
+      type: String,
+      enum: ['user', 'admin'],
+      default: 'user',
+    },
+    image: {
+      type: String, // Used by NextAuth (Google login etc)
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
+
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+
+  next();
+});
+
+const User = models.User || model('User', userSchema);
+export default User;
