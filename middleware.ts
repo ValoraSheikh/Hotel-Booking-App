b501866a5488d2678cbad859981007cbe2678f4e
Aman Sheikh <haarishsheikh04@gmail.com>
2025-07-05 10:17:19 +0530

    Create a middleware and register route to register new user

---
+++ middleware.ts
@@ -0,0 +1,24 @@
+// app/middleware.ts
+
+import { getToken } from "next-auth/jwt";
+import { NextRequest, NextResponse } from "next/server";
+
+// Define protected routes
+const protectedRoutes = ["/dashboard", "/booking"];
+
+export async function middleware(req: NextRequest) {
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+
+  const { pathname } = req.nextUrl;
+
+  const isProtected = protectedRoutes.some((route) =>
+    pathname.startsWith(route)
+  );
+
+  if (isProtected && !token) {
+    const loginUrl = new URL("/sign-in", req.url);
+    return NextResponse.redirect(loginUrl);
+  }
+
+  return NextResponse.next();
+}
