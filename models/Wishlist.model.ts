d7d23de6be6eedf5c24baa669026118b8375faae
Aman Sheikh <haarishsheikh04@gmail.com>
2025-07-25 17:18:50 +0530

    Create a Wishlist model for mongodb

---
+++ models/Wishlist.model.ts
@@ -0,0 +1,27 @@
+import { model, models, Schema, Types } from "mongoose";
+
+interface IWishlist {
+  user: Types.ObjectId;
+  roomId: Types.ObjectId;
+}
+
+const wishlistSchema = new Schema(
+  {
+    user: {
+      type: Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    roomId: {
+      type: Types.ObjectId,
+      ref: "Room",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Wishlist =
+  models?.Wishlist || model<IWishlist>("Wishlist", wishlistSchema);
+
+export default Wishlist;
